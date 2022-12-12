9eec621287a50a93e3dd76711fa64acb5e23c380
Artur Bajll <artur.bajll@orange.com>
2022-12-12 16:18:39 +0100

    recipe list API fix

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -17,6 +17,7 @@ from recipe.serializers import RecipeSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
+
 def create_recipe(user, **params):
     """Create and return a sample recipe."""
     defaults = {
